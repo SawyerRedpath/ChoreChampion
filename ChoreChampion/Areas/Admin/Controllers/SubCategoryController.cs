17513b058816bcbc8ba156639919e51de0a75b77
Sawyer Redpath <sawyerbruceredpath@gmail.com>
2019-12-15 14:42:20 -0500

    Work on subcategory, added logo, etc.

--- ChoreChampion/Areas/Admin/Controllers/SubCategoryController.cs
+++ ChoreChampion/Areas/Admin/Controllers/SubCategoryController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ChoreChampion.Data;
+using ChoreChampion.Models;
 using ChoreChampion.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace ChoreChampion.Areas.Admin.Controllers
@@ -13,6 +15,10 @@ namespace ChoreChampion.Areas.Admin.Controllers
     public class SubCategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        // TempData is great for showing status messages, after the message is closed or the page is refreshed, the message dissapears.
+        [TempData]
+        public string StatusMessage { get; set; }
 
         public SubCategoryController(ApplicationDbContext db)
         {
@@ -41,6 +47,53 @@ namespace ChoreChampion.Areas.Admin.Controllers
             };
 
             return View(model);
+        }
+
+        // POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Get all the records where the name matches the models name, and is contained in the same category
+                var subCategoryExists = _db.SubCategory.Include(s => s.Category).Where(s => s.Name == model.SubCategory.Name && s.Category.Id == model.SubCategory.CategoryId);
+
+                // If the subcategory already exists in this given category
+                if (subCategoryExists.Count() > 0)
+                {
+                    // Error, not allowed
+                    StatusMessage = "Error: Another Sub Category with this name exists under " + subCategoryExists.First().Category.Name + " category. Please use a different name.";
+                }
+                else
+                {
+                    _db.SubCategory.Add(model.SubCategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync(),
+                StatusMessage = StatusMessage
+            };
+            return View(modelVM);
+        }
+
+        [ActionName("GetSubCategory")]
+        public async Task<IActionResult> GetSubCategory(int id)
+        {
+            List<SubCategory> subCategories = new List<SubCategory>();
+
+            // Select each subcategory in the subcategory db where the categoryid matches the id passed in
+            subCategories = await (from subCategory in _db.SubCategory
+                                   where subCategory.CategoryId == id
+                                   select subCategory).ToListAsync();
+
+            return Json(new SelectList(subCategories, "Id", "Name"));
         }
     }
 }
