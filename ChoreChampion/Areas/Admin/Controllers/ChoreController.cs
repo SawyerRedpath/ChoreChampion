00641ab1384ee79f994bbc65f244e1d650776476
Sawyer Redpath <sawyerbruceredpath@gmail.com>
2020-01-10 18:21:14 -0500

    Finished chore post method

--- ChoreChampion/Areas/Admin/Controllers/ChoreController.cs
+++ ChoreChampion/Areas/Admin/Controllers/ChoreController.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ChoreChampion.Data;
 using ChoreChampion.Models;
 using ChoreChampion.Models.ViewModels;
+using ChoreChampion.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
 
 namespace ChoreChampion.Areas.Admin.Controllers
 {
@@ -16,7 +17,7 @@ namespace ChoreChampion.Areas.Admin.Controllers
     public class ChoreController : Controller
     {
         private readonly ApplicationDbContext _db;
-        private readonly IHostEnvironment _hostingEnvironment;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
         [BindProperty]
         public ChoreViewModel ChoreVM { get; set; }
@@ -43,6 +44,58 @@ namespace ChoreChampion.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View(ChoreVM);
+        }
+
+        // POST - CREATE
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        // The menu item viewmodel does not need to be passed to the method because I have used the BindProperty above
+        public async Task<IActionResult> CreatePOST()
+        {
+            ChoreVM.Chore.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (ModelState.IsValid)
+            {
+                _db.Chore.Add(ChoreVM.Chore);
+                await _db.SaveChangesAsync();
+
+                // Image saving below
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                // Uploaded files from the form (will only be one)
+                var files = HttpContext.Request.Form.Files;
+
+                // The chore that was just added to the database
+                var choreFromDb = await _db.Chore.FindAsync(ChoreVM.Chore.Id);
+
+                // File has been uploaded
+                if (files.Count > 0)
+                {
+                    var uploads = Path.Combine(webRootPath, "images");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStream = new FileStream(Path.Combine(uploads, ChoreVM.Chore.Id + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+                    choreFromDb.BeforeImage = @"\images\" + ChoreVM.Chore.Id + extension;
+                }
+
+                // No file uploaded, use default
+                else
+                {
+                    var uploads = Path.Combine(webRootPath, @"images\" + StaticDetails.PlaceHolderImage);
+                    System.IO.File.Copy(uploads, webRootPath + @"\images\" + ChoreVM.Chore.Id + ".png");
+                    choreFromDb.BeforeImage = @"\images\" + ChoreVM.Chore.Id + ".png";
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(ChoreVM);
+            }
         }
     }
 }
---
+++ ChoreChampion/Utility/StaticDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChoreChampion.Utility
+{
+    public class StaticDetails
+    {
+        public const string PlaceHolderImage = "No-Image-Available1.png";
+    }
+}
