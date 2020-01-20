c574f7f240e024b661d7bedce2e645738206905c
Sawyer Redpath <28635710+SawyerRedpath@users.noreply.github.com>
2020-01-20 11:48:01 -0500

    Added functionality to have random background selected on page load.

--- ChoreChampion/wwwroot/js/site.js
+++ ChoreChampion/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+        SelectRandomBackground();
+});
+
+function SelectRandomBackground() {
+    let index = Math.floor(Math.random() * 9);
+    $('body').attr('class', 'darkened-background' + index);
+}
